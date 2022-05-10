2661f97c5bc07dd9df002a5eb423651038909155
ramons <teco@ramons.digital>
2022-05-10 15:00:13 -0300

    fix mensaje de ingreso

--- clase 1/script.js
+++ clase 1/script.js
@@ -1,4 +1,4 @@
 console.log('Hola');
-let palabra = prompt('Ingrese una letra');
+let palabra = prompt('Ingrese una palabra');
 let cantidadLetras = palabra.length;
 console.log('La palabra ingresada tiene: ' + cantidadLetras + ' letras');
